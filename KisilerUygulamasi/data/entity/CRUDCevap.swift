bb4bf7d2263fb7ab99d974f407c7bdf7d4e35350
aliyzbsi <aliyzbsi68@hotmail.com>
2025-04-22 00:28:14 +0300

    Initial Commit

---
+++ KisilerUygulamasi/data/entity/CRUDCevap.swift
@@ -0,0 +1,15 @@
+//
+//  CRUDCevap.swift
+//  KisilerUygulamasi
+//
+//  Created by Kasım on 21.04.2025.
+//
+
+import Foundation
+
+class CRUDCevap:Codable {
+    
+    var success:Int!
+    var message:String!
+    
+}
---
+++ KisilerUygulamasi/data/entity/KisilerCevap.swift
@@ -0,0 +1,13 @@
+//
+//  KisilerCevap.swift
+//  KisilerUygulamasi
+//
+//  Created by Ali Yüzbaşı on 21.04.2025.
+//
+
+import Foundation
+
+class KisilerCevap :Codable{
+    var kisiler:[Kisiler]?
+    var success:Int?
+}
